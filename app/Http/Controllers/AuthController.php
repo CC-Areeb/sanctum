598d61c58f3cb41ed5990518b165d6778327014e
areeb <areeb.malik@cooperativecomputing.com>
2022-12-06 18:13:16 +0500

    Laravel sanctum basic setup and work

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     use HttpResponses;
+
+    public function login(LoginUserRequest $request)
+    {
+        $request->validated($request->all());
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return $this->error('', 401, 'User credentails did not match');
+        }
+
+        $user = User::where('email', $request->email)->first();
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API token of ' . $user->name)->plainTextToken
+        ]);
+    }
+
+    public function register(StoreUserRequest $request)
+    {
+        $request->validated($request->all());
+        $userData = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ];
+        $user = User::create($userData);
+
+        $successData = [
+            'user' => $user,
+            'token' => $user->createToken('API token of ' . $user->name)->plainTextToken
+        ];
+        return $this->success($successData);
+    }
+
+    public function logout()
+    {
+        return response()->json('This is the logout method');
+    }
 }
